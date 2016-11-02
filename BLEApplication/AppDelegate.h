d808af46df5164c4a4762b659abfef2c29b2dd50
Apple <apple@mac-mini.local>
2016-11-02 14:04:12 +0800

    Initial Commit

---
+++ BLEApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEApplication
+//
+//  Created by Apple on 16/11/2.
+//  Copyright © 2016年 Tsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
